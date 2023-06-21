6b02f6e8d52dec2a864ae5c6c77fc7e3a7c278a1
Mayhikall <mayhikalf99@gmail.com>
2023-06-21 22:22:57 +0700

    first commit

---
+++ src/pages/Error.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import { Link, useRouteError } from 'react-router-dom';
+import img from '../assets/not-found.svg';
+const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find page you are looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return <Wrapper>
+    <h3>something went wrong</h3>
+  </Wrapper>;
+};
+
+export default Error;
